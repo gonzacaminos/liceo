92a36c1d74db87815ba551e4bf25f5799961a50b
gonzacaminos <5827300+gonzalobonini@users.noreply.github.com>
2021-10-02 19:56:32 -0300

    Responsive & functional fixes

---
+++ components/circulo/index.js
@@ -0,0 +1,49 @@
+import { useState, useRef } from 'react';
+import SVG from 'react-inlinesvg';
+import styles from './circulo.module.scss'
+
+
+export default function Circulo(props){
+
+
+    const [selected, setSelected] = useState([false, false])
+    const circulo = useRef()
+
+    function handleClick(e){
+        const paths = circulo.current.querySelectorAll('svg,path')
+        const target = e.target
+        let _selected = [...selected]
+        const _color = {hex: target.getAttribute('fill') }
+
+        if(!e.target.tagName == "path" || !_color) {
+            return false
+        }
+
+        if(e.shiftKey) {
+           _selected[0] = target
+           props.setColor(_color)
+        } else {
+            _selected[1] = target
+            props.setFondo(_color)
+        }
+
+        setSelected(_selected)     
+
+        paths.forEach(path => (
+          path.style["filter"] = ""
+        ))
+
+        _selected.filter(path => {return path}).forEach(path => (
+            path.style["filter"] = "brightness(1.5)"
+        ))
+    
+        
+      }
+
+    return(
+        <div className={`${styles.circulo} mx-auto`} onClick={handleClick} ref={circulo}>
+             <SVG src={'/images/circulo.svg'} width={300} height={300} title="Círculo cromático" />
+        </div>
+    )
+    
+}
--- pages/index.js
+++ pages/index.js
@@ -1,48 +1,45 @@
-import {useState} from 'react'
+import {useState, useRef} from 'react'
 import Layout from '@/components/layout'
 import { PhotoshopPicker, SketchPicker } from 'react-color'
-import SVG from 'react-inlinesvg';
+import Circulo from '@/components/circulo'
 
 export default function Home() {
+
   const titulo = "Hola, chicxs!"
   const texto = "Veamos qué tan fácil es hacer que un texto se lea sobre un fondo y qué podemos hacer al respecto."
   const [color, setColor] = useState("#D50E0E")
   const [fondo, setFondo] = useState("#eeeeee")
+  const circulo = useRef()
 
-  function changeColor(color, event) {
+  function changeColor(color) {
     setColor(color.hex)
   }
 
-  function changeFondo(color, event) {
+  function changeFondo(color) {
     setFondo(color.hex)
   }
 
-  function handleClick(e){
-    const _color = e.target.getAttribute('fill').toLowerCase()
-    console.log(_color)
-    if(_color.includes('#')) {
-      setColor(_color)
-    }
+ 
 
-  }
 
   return (
     <Layout style={{background: fondo, color: color}}>
-      <div className="max-w-6xl mx-auto flex flex-col h-screen py-10">
+      <div className="max-w-6xl mx-auto flex flex-col lg:h-screen px-4 py-10">
         <input type="text" className="text-6xl font-bold mb-10 bg-transparent" defaultValue={titulo}/>
         <textarea className="text-xl bg-transparent" defaultValue={texto}/>
-        <div className="mt-auto">
-          <div className="flex justify-between">
-            <div>
+        <div className="mt-10 lg:mt-auto">
+          <div className="lg:flex justify-between">
+            <div className="p-4 mx-auto">
               <p className="text-base text-center mb-4">Color de fondo: {fondo}</p>
-              <SketchPicker color={fondo} onChangeComplete={changeFondo }/>
+              <SketchPicker color={fondo} onChangeComplete={changeFondo } className="mx-auto"/>
             </div>
-            <div onClick={handleClick}>
-             <SVG src={'/images/circulo.svg'} width={300} height="auto" title="Círculo cromático" />
+            <div className="p-4 mx-auto">
+            <p className="text-base text-center mb-4">Shift para cambiar el color de texto</p>
+             <Circulo setFondo={changeFondo} setColor={changeColor}/>
             </div>
-            <div>
+            <div className="p-4 mx-auto">
               <p className="text-base text-center mb-4">Color de texto: {color}</p>
-              <SketchPicker color={color} onChangeComplete={changeColor }/>
+              <SketchPicker color={color} onChangeComplete={changeColor} className="mx-auto"/>
             </div>
             
           </div>
